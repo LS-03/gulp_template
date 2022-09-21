f4d1b479ba9df834717d38ed2860b94029824c6b
Saveliy <lebedev-saveliy@mail.ru>
2022-09-21 14:18:23 +0300

    Fonts support added

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 
 
 // Наблюдатель за изменениями в файлах
@@ -27,7 +28,11 @@ function watcher() {
     gulp.watch(path.watch.images, images);
 }
 
-const mainTasks = gulp.parallel(copy, html, scss, js, images);
+// Поледовательная обработка шрифтов
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
+// Основные задачи
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
 
 // Построение сценариев выполнения задач
 //  series - последовательное выполнение задач
